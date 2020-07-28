58133e82d73584e34cb77315423865db0e1ffcf7
WarrenMfg <warrenmfg@icloud.com>
2020-07-28 15:28:19 -0400

    switch from topic localStorage to url query string

--- client/assets/js/topicCollection.js
+++ client/assets/js/topicCollection.js
@@ -1,9 +1,11 @@
 function topicCollection() {
-  const topicCollection = DOMPurify.sanitize(localStorage.getItem('topic').trim());
-  $('title').text(`Engineering Journal | ${topicCollection}`);
-  $('meta[property="og:title"]').attr('content', `Engineering Journal | ${topicCollection}`);
-  $('meta[name="twitter:title"]').attr('content', `Engineering Journal | ${topicCollection}`);
-  $('h1.page-title').text(topicCollection);
+  const collection = getURLcollection();
+  const topic = DOMPurify.sanitize(collection);
+
+  $('title').text(`Engineering Journal | ${topic}`);
+  $('meta[property="og:title"]').attr('content', `Engineering Journal | ${topic}`);
+  $('meta[name="twitter:title"]').attr('content', `Engineering Journal | ${topic}`);
+  $('h1.page-title').text(topic);
 }
 
 topicCollection();
